b634134a030e887e62cf815d906f55ada5fcc1d5
Veguista <pgv200080@gmail.com>
2026-01-07 17:36:01 +0000

    Fixed movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField][Range(0, 20)] float maxMovementSpeed = 8;
     [SerializeField][Range(0, 20)] float movementAcceleration = 7;
+    [SerializeField][Range(0, 5)] float movementResistance = 1.3f;
 
     Vector3 moveSpeed;
 
@@ -23,6 +24,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Applying move resistance.
+        moveSpeed *= 1.0f - Mathf.Clamp01(movementResistance * Time.deltaTime);
+
+        // New move input
         Vector2 regularMovement = playerMoveInputAction.ReadValue<Vector2>();
         float verticalMovement = playerVerticalMoveInputAction.ReadValue<float>();
 
@@ -30,20 +35,12 @@ public class PlayerMovement : MonoBehaviour
             + cameraTransform.right * regularMovement.x
             + cameraTransform.up * verticalMovement;
 
-        if(movementDir != Vector3.zero) // We accelerate
-        {
-            moveSpeed += movementDir * movementAcceleration * Time.deltaTime;
+        // We accelerate
+        moveSpeed += movementDir * movementAcceleration * Time.deltaTime;
             
-            // Clamping the move Speed.
-            moveSpeed = Vector3.ClampMagnitude(moveSpeed, maxMovementSpeed);
-        }
-        else // We reduce speed.
-        {
-            moveSpeed = moveSpeed.sqrMagnitude <= Mathf.Pow(movementAcceleration, 2) 
-                ? Vector3.zero 
-                : moveSpeed - moveSpeed.normalized * movementAcceleration * Time.deltaTime;
-        }
-
+        // Clamping the move Speed.
+        moveSpeed = Vector3.ClampMagnitude(moveSpeed, maxMovementSpeed);
+        
         transform.position += moveSpeed * Time.deltaTime;
     }
 }
